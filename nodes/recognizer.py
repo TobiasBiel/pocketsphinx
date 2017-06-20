d4ad48f3d09b52af81fabb4e3198a76c9ba72e19
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2017-06-20 15:11:01 +0200

    ros kinetic compatible, usage of gstreamer1.0 implemented with the help of https://cmusphinx.github.io/wiki/gstreamer/, also now publishes confidence

--- nodes/recognizer.py
+++ nodes/recognizer.py
@@ -4,34 +4,37 @@
 recognizer.py is a wrapper for pocketsphinx.
   parameters:
     ~lm - filename of language model
-    ~dict - filename of dictionary
     ~mic_name - set the pulsesrc device name for the microphone input.
                 e.g. a Logitech G35 Headset has the following device name: alsa_input.usb-Logitech_Logitech_G35_Headset-00-Headset_1.analog-mono
                 To list audio device info on your machine, in a terminal type: pacmd list-sources
   publications:
     ~output (std_msgs/String) - text output
+    ~confidence (std_msgs/Float32) - confidence
   services:
     ~start (std_srvs/Empty) - start speech recognition
     ~stop (std_srvs/Empty) - stop speech recognition
 """
-
+###
 import roslib; roslib.load_manifest('pocketsphinx')
 import rospy
 
-import pygtk
-pygtk.require('2.0')
+from gi import pygtkcompat
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst
+GObject.threads_init()
+Gst.init(None)
+gst = Gst
+pygtkcompat.enable()
+pygtkcompat.enable_gtk(version='3.0')
 import gtk
 
-import gobject
-import pygst
-pygst.require('0.10')
-gobject.threads_init()
-import gst
-
 from std_msgs.msg import String
+from std_msgs.msg import Float32
 from std_srvs.srv import *
 import os
 import commands
+
 
 class recognizer(object):
     """ GStreamer based speech recognizer. """
@@ -40,32 +43,17 @@ class recognizer(object):
         # Start node
         rospy.init_node("recognizer")
 
-        self._device_name_param = "~mic_name"  # Find the name of your microphone by typing pacmd list-sources in the terminal
         self._lm_param = "~lm"
         self._dic_param = "~dict"
 
-        # Configure mics with gstreamer launch config
-        if rospy.has_param(self._device_name_param):
-            self.device_name = rospy.get_param(self._device_name_param)
-            self.device_index = self.pulse_index_from_name(self.device_name)
-            self.launch_config = "pulsesrc device=" + str(self.device_index)
-            rospy.loginfo("Using: pulsesrc device=%s name=%s", self.device_index, self.device_name)
-        elif rospy.has_param('~source'):
-            # common sources: 'alsasrc'
-            self.launch_config = rospy.get_param('~source')
-        else:
-            self.launch_config = 'gconfaudiosrc'
-
-        rospy.loginfo("Launch config: %s", self.launch_config)
-
-        self.launch_config += " ! audioconvert ! audioresample " \
-                            + '! vader name=vad auto-threshold=true ' \
-                            + '! pocketsphinx name=asr ! fakesink'
+        self.launch_config = 'autoaudiosrc ! audioconvert ! audioresample ' \
+                             + '! pocketsphinx name=asr ! fakesink'
 
         # Configure ROS settings
         self.started = False
         rospy.on_shutdown(self.shutdown)
-        self.pub = rospy.Publisher('~output', String)
+        self.pub = rospy.Publisher('~output', String, queue_size=10)
+        self.conf_pub = rospy.Publisher('~confidence', Float32, queue_size=10)
         rospy.Service("~start", Empty, self.start)
         rospy.Service("~stop", Empty, self.stop)
 
@@ -79,10 +67,6 @@ class recognizer(object):
 
         self.pipeline = gst.parse_launch(self.launch_config)
         self.asr = self.pipeline.get_by_name('asr')
-        self.asr.connect('partial_result', self.asr_partial_result)
-        self.asr.connect('result', self.asr_result)
-        self.asr.set_property('configured', True)
-        self.asr.set_property('dsratio', 1)
 
         # Configure language model
         if rospy.has_param(self._lm_param):
@@ -100,19 +84,12 @@ class recognizer(object):
         self.asr.set_property('lm', lm)
         self.asr.set_property('dict', dic)
 
-        self.bus = self.pipeline.get_bus()
-        self.bus.add_signal_watch()
-        self.bus_id = self.bus.connect('message::application', self.application_message)
-        self.pipeline.set_state(gst.STATE_PLAYING)
+        bus = self.pipeline.get_bus()
+        bus.add_signal_watch()
+        self.bus_id = bus.connect('message::element', self.element_message)
+
+        self.pipeline.set_state(gst.State.PLAYING)
         self.started = True
-
-    def pulse_index_from_name(self, name):
-        output = commands.getstatusoutput("pacmd list-sources | grep -B 1 'name: <" + name + ">' | grep -o -P '(?<=index: )[0-9]*'")
-
-        if len(output) == 2:
-            return output[1]
-        else:
-            raise Exception("Error. pulse index doesn't exist for name: " + name)
 
     def stop_recognizer(self):
         if self.started:
@@ -123,7 +100,7 @@ class recognizer(object):
 
     def shutdown(self):
         """ Delete any remaining parameters so they don't affect next launch """
-        for param in [self._device_name_param, self._lm_param, self._dic_param]:
+        for param in [self._lm_param, self._dic_param]:
             if rospy.has_param(param):
                 rospy.delete_param(param)
 
@@ -140,38 +117,30 @@ class recognizer(object):
         rospy.loginfo("recognizer stopped")
         return EmptyResponse()
 
-    def asr_partial_result(self, asr, text, uttid):
-        """ Forward partial result signals on the bus to the main thread. """
-        struct = gst.Structure('partial_result')
-        struct.set_value('hyp', text)
-        struct.set_value('uttid', uttid)
-        asr.post_message(gst.message_new_application(asr, struct))
+    def element_message(self, bus, msg):
+        """Receive element messages from the bus."""
+        msg_type = msg.get_structure().get_name()
+        if msg_type != 'pocketsphinx':
+            return
 
-    def asr_result(self, asr, text, uttid):
-        """ Forward result signals on the bus to the main thread. """
-        struct = gst.Structure('result')
-        struct.set_value('hyp', text)
-        struct.set_value('uttid', uttid)
-        asr.post_message(gst.message_new_application(asr, struct))
+        if msg.get_structure().get_value('final'):
+            self.final_result(msg.get_structure().get_value('hypothesis'), msg.get_structure().get_value('confidence'))
+        elif msg.get_structure().get_value('hypothesis'):
+            self.partial_result(msg.get_structure().get_value('hypothesis'))
 
-    def application_message(self, bus, msg):
-        """ Receive application messages from the bus. """
-        msgtype = msg.structure.get_name()
-        if msgtype == 'partial_result':
-            self.partial_result(msg.structure['hyp'], msg.structure['uttid'])
-        if msgtype == 'result':
-            self.final_result(msg.structure['hyp'], msg.structure['uttid'])
-
-    def partial_result(self, hyp, uttid):
+    def partial_result(self, hyp):
         """ Delete any previous selection, insert text and select it. """
         rospy.logdebug("Partial: " + hyp)
 
-    def final_result(self, hyp, uttid):
+    def final_result(self, hyp, confidence):
         """ Insert the final result. """
         msg = String()
         msg.data = str(hyp.lower())
-        rospy.loginfo(msg.data)
+        conf = Float32()
+        conf.data = float(confidence)
+        rospy.loginfo("Final: " + msg.data + ", with confidence: " + str(confidence))
         self.pub.publish(msg)
+        self.conf_pub.publish(conf)
 
 if __name__ == "__main__":
     start = recognizer()
--- nodes/voice_cmd_vel.py
+++ nodes/voice_cmd_vel.py
@@ -13,6 +13,7 @@ import math
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
+
 class voice_cmd_vel:
 
     def __init__(self):
@@ -21,49 +22,50 @@ class voice_cmd_vel:
         self.msg = Twist()
 
         # publish to cmd_vel, subscribe to speech output
-        self.pub_ = rospy.Publisher('cmd_vel', Twist)
+        self.pub_ = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
         rospy.Subscriber('recognizer/output', String, self.speechCb)
 
-        r = rospy.Rate(10.0)
+        r = rospy.Rate(1.0)
         while not rospy.is_shutdown():
             self.pub_.publish(self.msg)
             r.sleep()
+        rospy.spin()
         
     def speechCb(self, msg):
-        rospy.loginfo(msg.data)
+        rospy.loginfo("Got data: " + msg.data)
 
-        if msg.data.find("full speed") > -1:
+        if "full speed" in msg.data or "fast" in msg.data:
             if self.speed == 0.2:
                 self.msg.linear.x = self.msg.linear.x*2
                 self.msg.angular.z = self.msg.angular.z*2
                 self.speed = 0.4
-        if msg.data.find("half speed") > -1:
+        if "half speed" in msg.data or "slow" in msg.data:
             if self.speed == 0.4:
                 self.msg.linear.x = self.msg.linear.x/2
                 self.msg.angular.z = self.msg.angular.z/2
                 self.speed = 0.2
 
-        if msg.data.find("forward") > -1:    
+        if "forward"in msg.data or "ahead" in msg.data or "move" in msg.data:
             self.msg.linear.x = self.speed
             self.msg.angular.z = 0
-        elif msg.data.find("left") > -1:
+        elif "left" in msg.data:
             if self.msg.linear.x != 0:
                 if self.msg.angular.z < self.speed:
                     self.msg.angular.z += 0.05
             else:        
                 self.msg.angular.z = self.speed*2
-        elif msg.data.find("right") > -1:    
+        elif "right" in msg.data:
             if self.msg.linear.x != 0:
                 if self.msg.angular.z > -self.speed:
                     self.msg.angular.z -= 0.05
             else:        
                 self.msg.angular.z = -self.speed*2
-        elif msg.data.find("back") > -1:
+        elif "back" in msg.data:
             self.msg.linear.x = -self.speed
             self.msg.angular.z = 0
-        elif msg.data.find("stop") > -1 or msg.data.find("halt") > -1:          
+        elif "stop" in msg.data or "halt" in msg.data:
             self.msg = Twist()
-        
+
         self.pub_.publish(self.msg)
 
     def cleanup(self):
